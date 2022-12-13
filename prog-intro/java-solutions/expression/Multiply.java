b1ff9f9e5926d790b9b356e490f7495e08935dbb
weldfy <114976687+weldfy@users.noreply.github.com>
2022-12-13 17:55:05 +0300

    Add files via upload

---
+++ prog-intro/java-solutions/expression/Multiply.java
@@ -0,0 +1,24 @@
+package expression;
+
+public class Multiply extends Sign {
+    public Multiply (Expression left, Expression right) {
+        super(left, right);
+    }
+    @Override
+    public char getSign() {
+        return '*';
+    }
+    @Override
+    public Number operation(Number left, Number right) {
+        return left.getClass() == Integer.class ?
+                left.intValue() * right.intValue() : left.doubleValue() * right.doubleValue();
+    }
+    @Override
+    public String getL() {
+        return "+-";
+    }
+    @Override
+    public String getR() {
+        return "+-/";
+    }
+}
